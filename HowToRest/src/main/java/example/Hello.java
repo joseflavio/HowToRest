43a55c3953af86e714a4e82bf5503f1f0e95db76
Jose Flavio AGUILAR <joseflavio@gmail.com>
2014-06-26 16:02:50 +0200

    Gradle build defaults and hot deploy

--- HowToRest/src/main/java/example/Hello.java
+++ HowToRest/src/main/java/example/Hello.java
@@ -8,23 +8,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 // Extend HttpServlet class
 public class Hello extends HttpServlet {
 
-	private static final long serialVersionUID = 5673733253605290120L;
-	
-	private final String message = "Hello world1234";
+    private static final long serialVersionUID = 5673733253605290120L;
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        // Set response content type
-        response.setContentType("text/html");
-        // Actual logic goes here.
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
-    }
+    private static final Logger logger = LoggerFactory.getLogger(Hello.class);
 
+    private static final String message = "Hello world1234";
+
+    @Override
     public void destroy() {
         // do nothing.
+    }
+
+    @Override
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.trace("doGet - enter");
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<h1>" + message + "</h1>");
+        }
     }
 }
